03ecfd0f32207a45f4bb809cab4257e7be1b7ba6
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-10 15:37:41 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
-
+import java.util.Scanner;
 public class Main {
-  
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      RWOnly obj = new RWOnly();
+      String S = sc.nextLine();
+      obj.setName(S);
+      String name = obj.getName();
+      System.out.println("You have entered: "+name);
+
+  }
 }
